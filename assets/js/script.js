5e83705fbd9cf021e728bd574ed10fd0d952ba29
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2023-01-12 22:34:08 -0300

    views

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const sectionUsersM = document.querySelector('#userManegement')
+
+function hideEdit() {
+    sectionUsersM.classList.remove('open')
+}
+
+function showEdit() {
+    sectionUsersM.classList.add('open')
+}
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express'),
       app = express()
 
+const path = require('path')
+
 const dotenv = require('dotenv')
       dotenv.config({ path : 'config.env'}),
       PORT = process.env.PORT || 4040
@@ -8,8 +10,21 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 app.use(morgan('tiny'))
 
+const bodyparser = require('body-parser')
+app.use(bodyparser.urlencoded({ extended : true }))
+
+app.set('view engine', 'hbs')
+
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+
 app.get('/', (req, res) => {
-    res.send("Crud Application")    
+    res.render('index')
+})
+
+app.get('/newuser', (req, res) => {
+    res.render('newuser')
 })
 
 app.listen(PORT, () => {console.log(`Server is running on http://localhost:${PORT}`)})
