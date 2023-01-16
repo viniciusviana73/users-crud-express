8d3fa40e30c142fd57fb7ee59cd78db3e6e8603a
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2023-01-16 13:33:54 -0300

    index - DB data

--- server/services/render.js
+++ server/services/render.js
@@ -1,6 +1,15 @@
+const axios = require('axios')
 
-exports.homeRoutes = (req, res) => {
-    res.render('index')
+exports.homeRoutes = async (req, res) => {
+    try {
+        const response = await axios.get(`http://localhost:${PORT}/api/users`),
+              users = response.data
+        //console.log(users)
+        res.render('index', { users: users })
+    } catch (error) {
+        res.send({message: `Error homeRoutes: ${error}`})
+        console.log(`Error homeRoutes: ${error}`)
+    }
 }
 
 exports.newUser = (req, res) => {
